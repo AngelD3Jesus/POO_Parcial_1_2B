c7b13fc0f3f190a0d6260c8d49c3a1e965b03bb2
AngelD3Jesus <Angel_3012230140@utd.edu.mx>
2024-06-12 08:59:53 -0600

    ER Repaso y Proyectos of VSCode

--- Unidad 2/14_proyectos/Cal.py
+++ Unidad 2/14_proyectos/Cal.py
@@ -18,7 +18,7 @@ def getCalculadora(num1, num2, operacion):
     elif operacion in ["5", "**", "POTENCIA"]:
         resultado = f"{num1} ** {num2} = {num1 ** num2}"
     elif operacion in ["6", "RAIZ", "RAIZ CUADRADA"]:
-        resultado = f"Raíz cuadrada de {num1} = {num1 ** 0.5}"
+        resultado = f"Raíz cuadrada de {num1} = {num1 ** 0.5} "
     else:
         resultado = "Operación no válida."
     return resultado
